f086e57865d6d3991623549c2632fbb0e8299476
mizamarzes <mizamarzes@gmail.com>
2024-08-24 16:49:02 -0500

    v1.0.0 - designing the database

---
+++ src/main/java/com/survey/survey/roles/domain/entities/Role.java
@@ -0,0 +1,33 @@
+package com.survey.survey.roles.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/survey/survey/users/domain/entities/User.java
@@ -0,0 +1,56 @@
+package com.survey.survey.users.domain.entities;
+
+import java.util.Set;
+
+import com.survey.survey.roles.domain.entities.Role;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "enabled", columnDefinition = "BOOL", nullable = false)
+    private boolean enabled;
+
+    @Column(name = "username", columnDefinition = "VARCHAR(12)", nullable = false)
+    private String username;
+
+    @Column(name = "password", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public User() {
+    }
+
+    public User(Long id, boolean enabled, String username, String password) {
+        this.id = id;
+        this.enabled = enabled;
+        this.username = username;
+        this.password = password;
+    }
+}
