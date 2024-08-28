0e0e5c807c500d02398a78f5b4049a77e5c6bbd9
mizamarzes <mizamarzes@gmail.com>
2024-08-28 16:07:11 -0500

    v1.0.2 response, subreponse

---
+++ src/main/java/com/survey/survey/response_question/domain/entities/ResponseQuestion.java
@@ -0,0 +1,40 @@
+package com.survey.survey.response_question.domain.entities;
+
+import com.survey.survey.response_options.domain.entities.ResponseOptions;
+import com.survey.survey.subresponse_options.domain.entities.SubResponseOptions;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "response_questions")
+public class ResponseQuestion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "subresponses_id", nullable = false)
+    private SubResponseOptions subResponseOptions;
+
+    @ManyToOne
+    @JoinColumn(name = "response_id", nullable = false)
+    private ResponseOptions responseOptions;
+
+    @Column(columnDefinition = "VARCHAR(80)", nullable = false)
+    private String responsetext;
+}
---
+++ src/main/java/com/survey/survey/subresponse_options/domain/entities/SubResponseOptions.java
@@ -0,0 +1,42 @@
+package com.survey.survey.subresponse_options.domain.entities;
+
+import com.survey.survey.response_options.domain.entities.ResponseOptions;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "subresponse_options")
+public class SubResponseOptions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "INTEGER", nullable = false)
+    private int subresponse_number;
+
+    @ManyToOne
+    @JoinColumn(name = "responseoptions_id", nullable = false)
+    private ResponseOptions responseOptions;
+
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    private String component_html;
+
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    private String subresponse_text;
+ 
+}
